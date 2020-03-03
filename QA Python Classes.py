89d4df8620c1156cfde59f019baed4480deb2d0e
Yasir Satti <martaniblett@gmail.com>
2020-03-03 11:05:06 +0000

    new machine

---
+++ QA Python Classes.py
@@ -0,0 +1,18 @@
+"""
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+John = Student ("John", "21")
+Jane = Student ("Jane", "22")
+
+print (getattr(John, "age"))
+
+"""
+
+class student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    c
